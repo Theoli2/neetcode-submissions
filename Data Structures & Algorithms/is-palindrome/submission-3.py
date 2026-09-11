8ae2c80a1bd226f3acf588d24efedc489df8d1b4
Theoli2 <105321343+Theoli2@users.noreply.github.com>
2026-09-12 01:51:25 +0200

    Add: is-palindrome - submission-3

---
+++ Data Structures & Algorithms/is-palindrome/submission-3.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        clean = ''.join(char for char in s if char.isalnum())
+        lower = clean.lower()
+        for left_char, right_char in zip(lower, reversed(lower)):
+            if (left_char != right_char):
+                return False
+
+        return True
